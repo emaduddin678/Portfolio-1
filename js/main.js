e586dceb00bbb1da7bd3eed00cb33b10711eba81
Emad Uddin <emaduddin678@gmail.com>
2023-01-07 00:41:49 +0600

    qualification done

--- js/main.js
+++ js/main.js
@@ -19,12 +19,57 @@ if (navClose) {
   });
 }
 
-
 // ========= REMOVE MENU MOBILE ==========
-const navLink = document.querySelectorAll('.nav_link');
+const navLink = document.querySelectorAll(".nav_link");
 
 function linkAction() {
-    // When we click on each nav_link, we remove the show-menu class 
-    navMenu.classList.remove('show-menu');
+  // When we click on each nav_link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// ================== SKILLS ==============
+const skillsContent = document.getElementsByClassName("skills_content");
+const skillsHeader = document.querySelectorAll(".skills_header");
+// skillsContent is a Array[1,2,3];
+// skills_header is a Array[1,2,3] of header under content 
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  // console.log(itemClass);
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills_content skills_close";
+  }
+  if (itemClass === "skills_content skills_close") {
+    this.parentNode.className = "skills_content skills_open";
+  }
+}
+
+skillsHeader.forEach((e) => {
+  e.addEventListener("click", toggleSkills);
+});
+
+// ======== QUALIFICATION TABS ==========
+const tabs = document.querySelectorAll("[data-target]");
+console.log(tabs);
+const tabContents = document.querySelectorAll("[data-content]");
+console.log(tabContents);
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    console.log(target);
+    // console.log(tabContents[0] === target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification_active");
+    });
+    target.classList.add("qualification_active");
+
+    tabs.forEach(tab => {
+      tab.classList.remove("qualification_active");
+    })
+    tab.classList.add("qualification_active");
+  });
+});
